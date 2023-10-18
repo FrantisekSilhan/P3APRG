88679b20f79cd3ac0e700da1ce533d65ffa3c4b7
František Šilhán <frantiseksilhann@gmail.com>
2023-10-18 07:57:06 +0000

    maui counter

--- Maui01First/MainPage.xaml.cs
+++ Maui01First/MainPage.xaml.cs
@@ -11,11 +11,28 @@
         private void btnDown_Clicked(object sender, EventArgs e)
         {
             lblCounter.Text = (--_value).ToString();
+            RedrawUI();
         }
 
         private void btnUp_Clicked(object sender, EventArgs e)
         {
             lblCounter.Text = (++_value).ToString();
+            RedrawUI();
+        }
+
+        private void btnSet_Clicked(object sender, EventArgs e)
+        {
+            if (sender is Button)
+            {
+                lblCounter.Text = (_value = Convert.ToInt32((sender as Button).Text)).ToString();
+            }
+            RedrawUI();
+        }
+
+        private void RedrawUI()
+        {
+            btnDown.IsEnabled = _value > 0;
+            btnUp.IsEnabled = _value < 100;
         }
     }
 }
