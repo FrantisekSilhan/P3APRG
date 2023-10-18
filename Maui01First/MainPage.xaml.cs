8aa380c081c0a842c9939f3a696bc4f991d6018f
František Šilhán <frantiseksilhann@gmail.com>
2023-10-18 07:21:05 +0000

    first maui

---
+++ Maui01First/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Maui01First
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _value = 7;
+        public MainPage()
+        {
+            InitializeComponent();
+            lblCounter.Text = _value.ToString();
+        }
+        private void btnDown_Clicked(object sender, EventArgs e)
+        {
+            lblCounter.Text = (--_value).ToString();
+        }
+
+        private void btnUp_Clicked(object sender, EventArgs e)
+        {
+            lblCounter.Text = (++_value).ToString();
+        }
+    }
+}
