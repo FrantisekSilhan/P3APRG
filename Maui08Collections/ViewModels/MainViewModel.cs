96cb2b1374d3c044df309f9bb1121755810a3948
František Šilhán <frantisek@slhn.cz>
2023-12-13 08:07:04 +0000

    ObservableCollection

--- Maui08Collections/ViewModels/MainViewModel.cs
+++ Maui08Collections/ViewModels/MainViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System.ComponentModel;
+﻿using Maui08Collections.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Maui08Collections.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<ShoppingItem> _items;
         private int _x;
-
+        public ObservableCollection<ShoppingItem> Items
+        {
+            get
+            {
+                return _items;
+            }
+            set
+            {
+                _items = value;
+                OnPropertyChanged();
+            }
+        }
         public int X
         {
             get
@@ -19,10 +33,23 @@ namespace Maui08Collections.ViewModels
                 OnPropertyChanged();
             }
         }
+        public Command CreateCommand { get; set; }
 
         public MainViewModel()
         {
+            Items = new ObservableCollection<ShoppingItem>();
+            Items.Add(new ShoppingItem { Id = 1, Name = "Borůvky" });
+            Items.Add(new ShoppingItem { Id = 2, Name = "Chléb" });
             X = Random.Shared.Next();
+            CreateCommand = new Command(
+            () =>
+            {
+                Items.Add(new ShoppingItem { Id = 1, Name = "Mléko" });
+            },
+            () =>
+            {
+                return true;
+            });
         }
 
         #region MVVM
